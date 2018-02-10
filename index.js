484a24853bc228ea0bc6fd888f8af67bed446161
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-02-10 23:46:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,23 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value});
 }
 
-function desctructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object.key = 'value';
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  var object = new Object();
+  object.key = '1';
   return object;
 }
 
 function deleteFromObjectByKey(object, key){
+  var object = new Object();
+  object.key = '1';
   var newObject = Object.assign({}, object);
-  delete. newObject.key;
+  delete newObject.key;
   return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  var object = new Object();
+  object.key = '1';
+  delete object.key;
+  return object;
 }
