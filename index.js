38fb580383738ce682ff8b4d64e90f61070cee58
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-02-11 00:09:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   var object = new Object();
   object.key = '1';
+  object.key = '2';
   return object;
 }
 
